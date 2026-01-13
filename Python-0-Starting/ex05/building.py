43b3ae58e7ad4d74d1736856f969eeef965bb324
heuchoi <chlgmdtn95@gmail.com>
2026-01-13 18:09:59 +0900

    m

---
+++ Python-0-Starting/ex05/building.py
@@ -0,0 +1,16 @@
+import sys
+
+def countString()
+
+def main() :
+	result = countString()
+	print(f"The text contains {result[0]} characters:\n")
+	print(f"{result[1]} upper letters\n")
+	print(f"{result[2]} lower letters\n")
+	print(f"{result[3]} punctuation marks\n")
+	print(f"{result[4]} spaces\n")
+	print(f"{result[5]} digits\n")
+
+if __name__ == "__main__" :
+	main()
+
