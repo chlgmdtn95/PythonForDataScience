313e4f05ae0b6ffd4dc3ab7cf65865d79709a05e
heuchoi <heuchoi@heuchoiui-Macmini.local>
2026-02-03 20:01:19 +0900

    ex08

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,17 +1,19 @@
-import os
+from os import get_terminal_size
 
 def forPrint(i:int, t:int) :
+	terminal_width = tuple(get_terminal_size())[0]
+	one_col = "█" * ((terminal_width - 30 - ((2 * len(str(t))))) // t)
+	one_blank = " " * ((terminal_width - 30 - ((2 * len(str(t))))) // t)
 	ret = ""
-	for _ in range(i-1):
-		ret += "="
-
-	ret+=">"
-	if i == t-1 :
-		print(f"\r100%|", end="")
-	else :
-		print(f"\r {i * 100//t }%|", end="")
-	print(f"{ret}", end="")
+	for _ in range(i):
+		ret += one_col
+	for _ in range(t-i, 0, -1):
+		ret += one_blank
+	ret += f"| {i}/{t}"
+	print(f"\r{i * 100//t :>3}%|".rjust(5), f"{ret}", end="", sep='', flush=True)
 
 def ft_tqdm(lst: range) -> None :
 	for i in range (len(lst)) :
-		yield	forPrint(i, len(lst))
+		forPrint(i, len(lst))
+		yield lst[i]
+	forPrint(len(lst), len(lst))
--- Python-0-Starting/ex08/test.py
+++ Python-0-Starting/ex08/test.py
@@ -2,10 +2,11 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-for elem in ft_tqdm(range(20)):
-	sleep(0.5)
+for elem in ft_tqdm(range(333)):
+	sleep(0.005)
+	# print("right after sleep")
 print()
 
-for elem in tqdm(range(20)):
-	sleep(0.5)
+for elem in tqdm(range(333)):
+	sleep(0.005)
 print()
