681c9389750fbaf6f0537b1c1cfe10e41af9206a
chlgmdtn95 <72033534+chlgmdtn95@users.noreply.github.com>
2026-02-05 13:11:03 +0000

    ex08 fin

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -2,15 +2,16 @@ from os import get_terminal_size
 
 def forPrint(i:int, t:int) :
 	terminal_width = tuple(get_terminal_size())[0]
-	one_col = "█" * ((terminal_width - 30 - ((2 * len(str(t))))) // t)
-	one_blank = " " * ((terminal_width - 30 - ((2 * len(str(t))))) // t)
-	ret = ""
-	for _ in range(i):
-		ret += one_col
-	for _ in range(t-i, 0, -1):
-		ret += one_blank
-	ret += f"| {i}/{t}"
-	print(f"\r{i * 100//t :>3}%|".rjust(5), f"{ret}", end="", sep='', flush=True)
+
+	left_ret = f"{i * 100 // t :>3}%|".rjust(5)
+	# right_ret = f"| {i}/{t} [00:01<00:00, 196.50it/s]"
+	right_ret = f"| {i}/{t}                          "
+	loading_bar_len = int((i / t) * (terminal_width - len(left_ret) - len(right_ret)))
+	mid_ret = "█" * loading_bar_len
+	mid_ret += " " * (terminal_width - len(left_ret) -len(right_ret) - loading_bar_len)
+
+	ret = left_ret + mid_ret + right_ret
+	print(f"\r{ret}", end="", flush=True)
 
 def ft_tqdm(lst: range) -> None :
 	for i in range (len(lst)) :
